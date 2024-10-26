3ccbc7554cf1f38a831128ad16c1e3860ddcba11
Harshal <harshalchaudhary03@gmail.com>
2024-10-26 13:11:54 +0530

    Statbox and little bit of Dashboard work

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,11 +1,28 @@
-import { Box } from "@mui/material";
+import { Box ,Button,IconButton,Typography,useTheme} from "@mui/material";
 import Header from "../../components/Header";
-
+import { tokens } from "../../Theme";
+import { mockTransactions } from "../../data/mockData";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import EmailIcon from "@mui/icons-material/Email";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import TrafficIcon from "@mui/icons-material/Traffic";
+import LineChart from "../../components/LineChart";
+import GeoGraphy from "../../components/GeoGraph";
+import BarChart from "../../components/BarChart";
+import StatBox from "../../components/StatBox";
+import ProgressCircle from "../../components/ProgressCircle";
 const Dashboard =()=>{
-    return <Box m="20px">
+    const theme=useTheme();
+    const colors=tokens(theme.palette.mode);
+    return( <Box m="20px">
         <Box display="flex" justifyContent="space-between" alignItems="center"> 
         <Header title="DASHBOARD" subtitle="Welcome to your dashboard"/>
-        </Box> 
+        </Box>
+        <Box>
+ 
+        </Box>
     </Box>
+    )
 }
 export default Dashboard;
